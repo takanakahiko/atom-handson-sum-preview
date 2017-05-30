e7ff9e03bcbf5bb7ded887184c396bbd6065633f
Nakahiko <takanakahiko@gmail.com>
2017-05-30 18:36:52 +0900

    init

---
+++ spec/atom-handson-sum-preview-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomHandsonSumPreviewView from '../lib/atom-handson-sum-preview-view';
+
+describe('AtomHandsonSumPreviewView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
